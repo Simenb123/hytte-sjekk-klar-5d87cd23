5007f4d18e2bc17070147ac780910443501b0652
Simenb123 <simenb89@gmail.com>
2025-07-12 01:23:38 +0200

    chore: remove unused supabase client

--- src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://oztvpaviseudhobxmzgi.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96dHZwYXZpc2V1ZGhvYnhtemdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2Njg0MTQsImV4cCI6MjA2MDI0NDQxNH0.t7fko5PqHOr5cXI0pnDEvKP4RYfEVgKr9Hn5ufiXS80';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
