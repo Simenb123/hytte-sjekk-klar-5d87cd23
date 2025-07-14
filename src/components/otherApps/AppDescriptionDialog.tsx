858fbe383dc74e0cf6dbbea93d27faca7836e27b
Simenb123 <simenb89@gmail.com>
2025-07-14 15:01:56 +0200

    Validate dialog trigger elements

--- src/components/otherApps/AppDescriptionDialog.tsx
+++ src/components/otherApps/AppDescriptionDialog.tsx
@@ -11,23 +11,40 @@ import {
 interface AppDescriptionDialogProps {
   title: string;
   description: string;
-  trigger: React.ReactNode;
+  /**
+   * A single React element used to open the dialog. Fragments are not supported.
+   */
+  trigger: React.ReactElement;
 }
 
+/**
+ * Dialog displaying information about an app.
+ *
+ * @param props.trigger - A single React element that opens the dialog. Must not be a React.Fragment.
+ */
 const AppDescriptionDialog: React.FC<AppDescriptionDialogProps> = ({
   title,
   description,
   trigger,
-}) => (
-  <Dialog>
-    <DialogTrigger asChild>{trigger}</DialogTrigger>
-    <DialogContent>
-      <DialogHeader>
-        <DialogTitle>{title}</DialogTitle>
-        <DialogDescription>{description}</DialogDescription>
-      </DialogHeader>
-    </DialogContent>
-  </Dialog>
-);
+}) => {
+  if (!React.isValidElement(trigger) || trigger.type === React.Fragment) {
+    console.error(
+      "AppDescriptionDialog: 'trigger' prop must be a single React element and not a React.Fragment"
+    );
+    return null;
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default AppDescriptionDialog;
