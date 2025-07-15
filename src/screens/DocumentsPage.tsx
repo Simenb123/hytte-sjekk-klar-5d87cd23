42d5aeadee9d014207173044921ebc52ad94bd09
Simenb123 <simenb89@gmail.com>
2025-07-15 13:36:42 +0200

    Center documents page content

--- src/screens/DocumentsPage.tsx
+++ src/screens/DocumentsPage.tsx
@@ -6,10 +6,8 @@ import DocumentsManager from '@/components/documents/DocumentsManager';
 export default function DocumentsPage() {
   return (
     <Layout title="Dokumenter" showBackButton>
-      <div className="flex-1 overflow-y-auto p-4 md:p-6">
-        <div className="w-full">
-          <DocumentsManager />
-        </div>
+      <div className="container mx-auto flex-1 overflow-y-auto p-4 md:p-6">
+        <DocumentsManager />
       </div>
     </Layout>
   );
